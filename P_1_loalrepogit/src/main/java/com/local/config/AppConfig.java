26aafa05c463b4c109961d1a5955de7c6a8b45da
Rahul <rajrahul18122003@gmail.com>
2026-04-24 17:18:47 +0530

    create config pack & AppFig class - pm

---
+++ P_1_loalrepogit/src/main/java/com/local/config/AppConfig.java
@@ -0,0 +1,5 @@
+package com.local.config;
+
+public class AppConfig {
+    
+}
---
+++ P_1_loalrepogit/src/main/java/com/local/config/MainApp.java
@@ -0,0 +1,10 @@
+package com.local.config;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		System.out.println("git local repo");
+
+	}
+
+}
