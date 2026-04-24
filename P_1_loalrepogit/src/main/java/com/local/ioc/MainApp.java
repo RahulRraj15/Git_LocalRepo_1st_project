c4113acdda02543b3d0ab30c86936d2ffa96e0f4
Rahul <rajrahul18122003@gmail.com>
2026-04-24 17:39:33 +0530

    commit 3rd time -- dev1

--- P_1_loalrepogit/src/main/java/com/local/ioc/MainApp.java
+++ P_1_loalrepogit/src/main/java/com/local/ioc/MainApp.java
@@ -5,7 +5,9 @@ public class MainApp {
 	public static void main(String[] args) {
 		System.out.println("git local repo");
 		System.out.println("now need to create IOC container");
-
+		System.out.println("how i need to create IOC mean with xml or annotation driven cfg");
+		
+        
 	}
 
 }
